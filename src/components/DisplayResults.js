714d84d3fe00d2bb88ae19eed15574b86d53ef2c
Gabriel Caramés <49535753+GabrielCarames@users.noreply.github.com>
2022-07-01 07:48:41 -0300

    bug fix

--- src/components/DisplayResults.js
+++ src/components/DisplayResults.js
@@ -12,26 +12,38 @@ import LocalMap from "./LocalMap"
 import Ads from "./Ads"
 
 const DisplayResults = () => {
-    const results = useSelector(state => state.resultsReducer).payload
-    const { ads, organic_results, knowledge_graph, top_stories, related_questions, related_searches, pagination, inline_tweets, local_map, local_results, inline_videos } = results
+  const results = useSelector(state => state.resultsReducer).payload
+  const {
+    ads,
+    organic_results,
+    knowledge_graph,
+    top_stories,
+    related_questions,
+    related_searches,
+    pagination,
+    inline_tweets,
+    local_map,
+    local_results,
+    inline_videos
+  } = results
 
-    return (
-        <div className="results-results-container">
-            <div className="results__left-section">
-                {top_stories && <TopStories topStories={top_stories} />}
-                {related_questions && <RelatedQuestions relatedQuestions={related_questions} />}
-                {ads && <Ads ads={ads} />}
-                {local_map && local_map.image && <LocalMap localMap={local_map} />}
-                {local_results && <LocalResults localResults={local_results} />} 
-                {inline_tweets && <InlineTweets inlineTweets={inline_tweets} />}
-                <OrganicResults organicResults={organic_results} />
-                {inline_videos && <InlineVideos inlineVideos={inline_videos} />}
-                {related_searches && <RelatedSearches relatedSearches={related_searches} />}
-                <Pagination pagination={pagination} />
-            </div>
-            {knowledge_graph && <KnowledgeGraph knowledgeGraph={knowledge_graph} />}
-        </div>
-    )
+  return (
+    <div className="results-results-container">
+      <div className="results__left-section">
+        {top_stories && <TopStories topStories={top_stories} />}
+        {related_questions && <RelatedQuestions relatedQuestions={related_questions} />}
+        {ads && <Ads ads={ads} />}
+        {local_map && local_map.image && <LocalMap localMap={local_map} />}
+        {local_results && <LocalResults localResults={local_results} />}
+        {inline_tweets && <InlineTweets inlineTweets={inline_tweets} />}
+        <OrganicResults organicResults={organic_results} />
+        {inline_videos && <InlineVideos inlineVideos={inline_videos} />}
+        {related_searches && <RelatedSearches relatedSearches={related_searches} />}
+        {pagination && <Pagination pagination={pagination} />}
+      </div>
+      {knowledge_graph && <KnowledgeGraph knowledgeGraph={knowledge_graph} />}
+    </div>
+  )
 }
 
 export default DisplayResults
